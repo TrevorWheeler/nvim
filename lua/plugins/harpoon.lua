077a5d61de036b9a70d4d981a0adb3d4399927a0
TrevorWheeler <sendtrevmail@gmail.com>
2024-01-30 22:43:12 +1000

    Revert "add bufferline"
    This reverts commit 4c7be7140298db7eb49a767fe0f86486ed3395e5.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ return {
   {
     'theprimeagen/harpoon',
     branch = 'harpoon2',
-    lazy = true,
+    lazy = false,
     config = true,
     keys = {
       {
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,19 +1,17 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
-  -- opts = {
-  --   start_in_insert = true,
-  --   direction = 'float',
-  --   open_mapping = [[<c-\>]],
-  -- },
-  config = function()
-    require('toggleterm').setup {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
       start_in_insert = true,
-      direction = 'float',
+      -- direction = 'float',
       -- open_mapping = [[<c-\>]],
-    }
-    vim.keymap.set('n', '<C-t>', ':ToggleTerm<CR>', { noremap = true, silent = true })
-    vim.keymap.set('t', '<C-t>', '<C-\\><C-n>:ToggleTerm<CR>', { noremap = true, silent = true })
-  end,
-}
+    },
+    config = function()
+      vim.keymap.set('n', '<C-t>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+      vim.keymap.set('t', '<C-t>', '<C-\\><C-n>:ToggleTerm<CR>', { noremap = true, silent = true })
+    end,
+  }
 -- {'akinsho/toggleterm.nvim', version = "*", config = true}
+
+}
